beb986ac93a1cdd8257a96baf811443eb945f5ac
Kelli White Weaver <kelliwhiteweaver@gmail.com>
2022-02-14 18:12:45 -0500

    if anything stops working then go to the one b4 this ok

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://whispering-meadow-54682.herokuapp.com/';
   }
 
   function getQueryStringParameterByName(name, url) {
