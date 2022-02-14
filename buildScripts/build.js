9ae9469c49ece919174b1e4284b7a629afd41300
Kelli White Weaver <kelliwhiteweaver@gmail.com>
2022-02-14 09:54:56 -0500

    fu

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -3,9 +3,13 @@ import webpack from "webpack";
 import webpackConfig from "../webpack.config.prod";
 import chalk from "chalk";
 
-process.env.NODE_ENV = 'production'; // this assures the Babel dev config doesn't apply.
+process.env.NODE_ENV = "production"; // this assures the Babel dev config doesn't apply.
 
-console.log(chalk.blue("Generating minified bundle for production. This will take a moment..."));
+console.log(
+  chalk.blue(
+    "Generating minified bundle for production. This will take a moment..."
+  )
+);
 
 webpack(webpackConfig).run((err, stats) => {
   if (err) {
